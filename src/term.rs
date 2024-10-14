9970010589ec2426e1616f84dbbd230b4d6818d8
lucasmolinari <lucas.molinari2003@gmail.com>
2024-10-14 14:08:38 -0300

    Enhance terminal init

--- src/term.rs
+++ src/term.rs
@@ -1,9 +1,10 @@
 use crossterm::{
+    cursor::{self},
     event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
     terminal::{Clear, ClearType},
 };
 use std::{
-    io::{self, Stdout, Write},
+    io::{self, BufWriter, Stdout, Write},
     time::Duration,
 };
 
@@ -14,14 +15,41 @@ use crossterm::{
 
 pub fn init(mut stdout: &Stdout) -> io::Result<()> {
     terminal::enable_raw_mode()?;
-    execute!(stdout, EnterAlternateScreen)?;
-    execute!(stdout, Clear(ClearType::All))?;
+    execute!(
+        stdout,
+        cursor::Hide,
+        EnterAlternateScreen,
+        Clear(ClearType::All),
+        cursor::MoveTo(0, 0),
+    )?;
+
+    let (cols, rows) = terminal::size()?;
+    let mut buffer = BufWriter::new(stdout);
+
+    for y in 0..rows {
+        buffer.write(b"~")?;
+        if y == rows / 3 {
+            let title = "Sage Text Editor";
+            let padding = (cols - title.len() as u16) / 2 - 1;
+            for _ in 0..=padding {
+                buffer.write(b" ")?;
+            }
+            buffer.write(title.as_bytes())?;
+        }
+        if y != rows - 1 {
+            buffer.write(b"\r\n")?;
+        }
+    }
+    buffer.flush()?;
+    execute!(stdout, cursor::MoveTo(1, 0), cursor::Show)?;
+
     Ok(())
 }
 
 pub fn destroy(mut stdout: &Stdout) -> io::Result<()> {
     terminal::disable_raw_mode()?;
     execute!(stdout, LeaveAlternateScreen)?;
+
     Ok(())
 }
 
