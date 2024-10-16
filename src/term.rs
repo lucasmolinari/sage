bbaa39d19b6237f0de73d7b2a15e364b7c473c8e
lucasmolinari <lucas.molinari2003@gmail.com>
2024-10-16 09:31:39 -0300

    Add rendering method + 1D grid

--- src/term.rs
+++ src/term.rs
@@ -6,6 +6,7 @@ use crossterm::{
 use std::{
     io::{self, BufWriter, Write},
     time::Duration,
+    usize,
 };
 
 use crossterm::{
@@ -19,64 +20,106 @@ enum Mode {
     Command,
 }
 
+type Point = (u16, u16);
+struct Grid {
+    chars: Vec<char>,
+    width: u16,
+    height: u16,
+}
+impl Grid {
+    fn new(width: u16, height: u16) -> Self {
+        let size = (width * height) as usize;
+
+        let mut chars = vec![' '; size];
+        for y in 0..height {
+            let index = (y * width) as usize;
+            chars[index] = '~';
+        }
+        Self {
+            chars,
+            width,
+            height,
+        }
+    }
+
+    fn get(&self, p: Point) -> &char {
+        let index = (p.1 * self.width + p.0) as usize;
+        &self.chars[index]
+    }
+
+    fn set(&mut self, p: Point, c: char) {
+        let index = (p.1 * self.width + p.0) as usize;
+        self.chars[index] = c;
+    }
+}
+
 pub struct Editor {
-    c_pos: (u16, u16),
-    orig_size: (u16, u16),
-    size: (u16, u16),
+    orig_size: Point,
+    size: Point,
     mode: Mode,
+    grid: Grid,
 }
 impl Editor {
     pub fn new() -> io::Result<Self> {
-        let size = terminal::size()?;
+        let orig_size = terminal::size()?;
         Ok(Editor {
-            c_pos: (0, 0),
-            orig_size: size,
-            size,
+            orig_size,
+            size: orig_size,
             mode: Mode::Normal,
+            grid: Grid::new(orig_size.0, orig_size.1),
         })
     }
     pub fn init(&mut self) -> io::Result<()> {
-        let stdout = io::stdout();
+        let mut stdout = io::stdout();
         terminal::enable_raw_mode()?;
         execute!(
-            &stdout,
-            cursor::Hide,
+            stdout,
             SetCursorStyle::BlinkingBlock,
             EnterAlternateScreen,
-            Clear(ClearType::All),
             cursor::MoveTo(0, 0),
         )?;
 
-        let (cols, rows) = self.size;
-        let mut buffer = BufWriter::new(&stdout);
+        self.render_screen()?;
+        execute!(stdout, cursor::MoveTo(1, 0))?;
+        Ok(())
+    }
 
-        for y in 0..rows {
-            buffer.write(b"~")?;
-            if y == rows / 3 {
-                let title = "Sage Text Editor";
-                let padding = (cols - title.len() as u16) / 2 - 1;
-                for _ in 0..=padding {
-                    buffer.write(b" ")?;
-                }
-                buffer.write(title.as_bytes())?;
-            }
-            if y != rows - 1 {
-                buffer.write(b"\r\n")?;
+    fn refresh_screen(&self) -> io::Result<()> {
+        let mut stdout = io::stdout();
+        execute!(stdout, Clear(ClearType::All))?;
+        Ok(())
+    }
+
+    fn render_screen(&self) -> io::Result<()> {
+        let stdout = io::stdout();
+
+        let c_pos = cursor::position()?;
+        execute!(&stdout, cursor::Hide, cursor::MoveTo(0, 0))?;
+
+        let mut buffer = BufWriter::new(&stdout);
+        for y in 0..self.grid.height {
+            for x in 0..self.grid.width {
+                let char = self.grid.get((x, y));
+                buffer.write(char.to_string().as_bytes())?;
             }
         }
         buffer.flush()?;
-        execute!(&stdout, cursor::MoveTo(1, 0), cursor::Show)?;
-        self.c_pos = (1, 0);
-
+        execute!(&stdout, cursor::MoveTo(c_pos.0 + 1, c_pos.1), cursor::Show)?;
         Ok(())
     }
 
     pub fn destroy(&self) -> io::Result<()> {
         let mut stdout = io::stdout();
+        self.refresh_screen()?;
         terminal::disable_raw_mode()?;
 
         let (rows, cols) = self.orig_size;
-        execute!(stdout, LeaveAlternateScreen, SetSize(rows, cols))?;
+        execute!(
+            stdout,
+            LeaveAlternateScreen,
+            SetSize(rows, cols),
+            SetCursorStyle::DefaultUserShape
+        )?;
 
         Ok(())
     }
@@ -109,11 +152,28 @@ impl Editor {
 
     fn handle_normal_press(&mut self, code: KeyCode) -> io::Result<()> {
         let mut stdout = io::stdout();
+        let c_pos = cursor::position()?;
         match code {
-            KeyCode::Char('h') => execute!(stdout, cursor::MoveLeft(1))?,
-            KeyCode::Char('l') => execute!(stdout, cursor::MoveRight(1))?,
-            KeyCode::Char('k') => execute!(stdout, cursor::MoveUp(1))?,
-            KeyCode::Char('j') => execute!(stdout, cursor::MoveDown(1))?,
+            KeyCode::Char('h') => {
+                if c_pos.0 != 1 {
+                    execute!(stdout, cursor::MoveLeft(1))?
+                }
+            }
+            KeyCode::Char('l') => {
+                if c_pos.0 != self.size.0 {
+                    execute!(stdout, cursor::MoveRight(1))?
+                }
+            }
+            KeyCode::Char('k') => {
+                if c_pos.1 != 0 {
+                    execute!(stdout, cursor::MoveUp(1))?
+                }
+            }
+            KeyCode::Char('j') => {
+                if c_pos.1 != self.size.1 {
+                    execute!(stdout, cursor::MoveDown(1))?
+                }
+            }
             KeyCode::Char('i') => {
                 execute!(stdout, cursor::MoveLeft(1))?;
                 self.change_mode(Mode::Insert)?;
@@ -128,13 +188,14 @@ impl Editor {
     }
 
     fn handle_insert_press(&mut self, code: KeyCode) -> io::Result<()> {
-        let mut stdout = io::stdout();
         match code {
             KeyCode::Esc => self.change_mode(Mode::Normal)?,
-            _ => {
-                write!(stdout, "{}", code)?;
-                stdout.flush()?;
+            KeyCode::Char(c) => {
+                let c_pos = cursor::position()?;
+                self.grid.set(c_pos, c);
+                self.render_screen()?;
             }
+            _ => {}
         };
         Ok(())
     }
